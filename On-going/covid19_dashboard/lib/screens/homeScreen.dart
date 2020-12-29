80322dc459d2660d957bc660e397fb893bb719a7
AbhilashTUofficial <abhilashstorm@gmail.com>
2020-12-29 22:01:56 +0530

    country dropdown

--- On-going/covid19_dashboard/lib/screens/homeScreen.dart
+++ On-going/covid19_dashboard/lib/screens/homeScreen.dart
@@ -1,15 +1,61 @@
+import 'package:covid19_dashboard/config/palette.dart';
+import 'package:covid19_dashboard/widgets/countryDropdown.dart';
 import 'package:covid19_dashboard/widgets/customAppBar.dart';
 import 'package:flutter/material.dart';
+
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String _country='USA';
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: CustomAppBar(),
+      body: CustomScrollView(
+        physics: ClampingScrollPhysics(),
+        slivers: [
+          _buildHeader(screenHeight),
+        ],
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildHeader(double screenHeight) {
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            color: Palette.primaryColor,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(40.0),
+              bottomRight: Radius.circular(40.0),
+            )),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "COVID-19",
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25.0),
+                ),
+                CountryDropdown(
+                  countries: ['CN','FR','IN','IT','UK','USA'],
+                  country: _country,
+                  onChanged: (val)=>setState(()=>_country=val),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- On-going/covid19_dashboard/lib/screens/screens.dart
+++ On-going/covid19_dashboard/lib/screens/screens.dart
@@ -1,3 +1,4 @@
 export 'bottomNavScreen.dart';
 export 'homeScreen.dart';
 export 'statsScreen.dart';
+export 'package:covid19_dashboard/widgets/countryDropdown.dart';

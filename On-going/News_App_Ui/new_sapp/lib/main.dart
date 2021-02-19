4bc73ab661b540aa8c39f4f4cc69aca7ad469ac0
AbhilashTUoffical <abhilashstorm@gmail.com>
2021-02-19 12:12:05 +0530

    ui run

---
+++ On-going/News_App_Ui/new_sapp/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'Screens/export.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:"NewsApp",
+      theme: ThemeData(
+        primarySwatch: Colors.white,
+      ),
+      home:Home(),
+    );
+  }
+}
+
+

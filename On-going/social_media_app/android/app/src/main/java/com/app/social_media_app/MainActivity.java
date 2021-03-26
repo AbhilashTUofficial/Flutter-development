a78775323e4e2d3a0d52819ddc9e31eff95ecc4c
AbhilashTUofficial <abhilashstorm@gmail.com>
2021-03-26 23:02:21 +0530

    update

--- On-going/social_media_app/android/app/src/main/java/com/app/social_media_app/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.app.social_media_app;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- On-going/social_media_app/lib/Screens/export.dart
+++
@@ -1,5 +0,0 @@
-export 'controller.dart';
-export 'home.dart';
-export 'liked.dart';
-export 'watch.dart';
-export 'news.dart';
